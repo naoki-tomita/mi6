2472116124bf38c74201036491def3fbc6b45b1d
Naoki Tomita🐱 <mxcpy608@hotmail.com>
2021-02-09 21:52:48 +0900

    Initial commit

---
+++ mod.test.ts
@@ -0,0 +1,76 @@
+import { assertStrictEquals, assertThrows, assertThrowsAsync } from "https://deno.land/std@0.86.0/testing/asserts.ts";
+import { createMock, mock } from "./mod.ts";
+
+Deno.test("mi6 > #createMock > can call.", () => {
+  const fn = createMock<[string, number], boolean>();
+  const bool: boolean = fn("string", 42);
+});
+
+Deno.test("mi6 > #createMock > mockReturnValueOnce returns undefined", () => {
+  const fn = createMock<[string, boolean], number>();
+
+  assertStrictEquals(fn("any", false), undefined);
+});
+
+Deno.test("mi6 > #createMock > mockReturnValueOnce", () => {
+  const fn = createMock<[string, boolean], number>();
+  fn.mockReturnValueOnce(42);
+
+  assertStrictEquals(fn("any", false), 42);
+});
+
+Deno.test("mi6 > #ccreateMock > mockResolveValueOnce", async () => {
+  const fn = createMock<[string, boolean], Promise<number>>();
+  fn.mockResolveValueOnce(42);
+
+  assertStrictEquals(await fn("any", false), 42);
+});
+
+Deno.test("mi6 > #ccreateMock > mockRejectValueOnce", async () => {
+  const fn = createMock<[string, boolean], Promise<number>>();
+  fn.mockRejectValueOnce(new Error("42"));
+
+  await assertThrowsAsync(() => fn("any", true), Error);
+});
+
+Deno.test("mi6 > #createMock > mockReturnValueOnce many time.", () => {
+  const fn = createMock<[string, boolean], number>();
+  fn.mockReturnValueOnce(12)
+    .mockReturnValueOnce(23)
+    .mockReturnValueOnce(42);
+
+  assertStrictEquals(fn("foo", true), 12);
+  assertStrictEquals(fn("bar", false), 23);
+  assertStrictEquals(fn("hoge", true), 42);
+  assertThrows(() => fn("aaa", false));
+});
+
+Deno.test("mi6 > #createMock > calledWith", () => {
+  const fn = createMock<[string, boolean], number>();
+  fn.calledWith("foo", false).mockReturnValueOnce(999);
+  assertStrictEquals(fn("foo", false), 999);
+});
+
+class Instance {
+  value: string = "hoge";
+}
+
+class FooClass {
+  method1(): number { throw Error() }
+  method2(arg1: string, arg2: number): string { throw Error() }
+  method3(): Instance { throw Error() }
+}
+
+Deno.test("mi6 > #mock > mock createable", () => {
+  const instance = mock<FooClass>();
+  instance.method1.mockReturnValueOnce(123);
+  instance.method2.calledWith("foo", 245).mockReturnValueOnce("foo245")
+  const x = new Instance();
+  instance.method3.calledWith().mockReturnValueOnce(x)
+
+  assertStrictEquals(instance.method1(), 123);
+  assertStrictEquals(instance.method2("foo", 245), "foo245");
+  assertStrictEquals(instance.method3(), x);
+})
+
+export {};
---
+++ mod.ts
@@ -0,0 +1,87 @@
+import { equal } from "https://deno.land/std@0.86.0/testing/asserts.ts";
+
+interface MockCalled<T extends any[], V> {
+  mockReturnValueOnce(returnValue: V): Mock<T, V>;
+  mockResolveValueOnce(resolvedValue: V extends Promise<infer U> ? U : never): Mock<T, V>;
+  mockRejectValueOnce(rejectedValue: any): Mock<T, V>;
+}
+
+interface Mock<T extends any[], V> extends MockCalled<T, V> {
+  (...args: T): V;
+  calledWith(...args: T): MockCalled<T, V>;
+  mock: {
+    calls: T[];
+  };
+}
+
+const DefaultArgs = [Symbol("default args")] as unknown as any;
+export function createMock<T extends any[], V>(): Mock<T, V> {
+  const stubs: Map<T, V[]> = new Map();
+  const calls: T[] = [];
+  const MockFn = (...args: T): V => {
+    calls.push(args);
+    if (stubs.size === 0) {
+      return undefined as unknown as any;
+    }
+    for (const [key, returnValue] of stubs.entries()) {
+      if (equal(key, args) && returnValue.length > 0) {
+        return returnValue.shift()!;
+      }
+    }
+    for (const [key, returnValue] of stubs.entries()) {
+      if (equal(key, DefaultArgs) && returnValue.length > 0) {
+        return returnValue.shift()!;
+      }
+    }
+    throw Error(`Not matched arguments. ${args.map(it => JSON.stringify(it)).join(", ")}`);
+  };
+  MockFn.mock = { calls };
+  const Mock: Mock<T, V> = MockFn as any;
+
+  const calledWith = Mock.calledWith = (...args): MockCalled<T, V> => {
+    return {
+      mockReturnValueOnce(returnValue) {
+        stubs.set(args, stubs.get(args) ?? []);
+        stubs.get(args)!.push(returnValue);
+        return Mock;
+      },
+      mockResolveValueOnce(resolvedValue) {
+        stubs.set(args, stubs.get(args) ?? []);
+        stubs.get(args)!.push(Promise.resolve(resolvedValue) as unknown as V);
+        return Mock;
+      },
+      mockRejectValueOnce(rejectedValue) {
+        stubs.set(args, stubs.get(args) ?? []);
+        stubs.get(args)!.push(Promise.reject(rejectedValue) as unknown as V);
+        return Mock;
+      }
+    };
+  };
+
+  const {
+    mockReturnValueOnce,
+    mockResolveValueOnce,
+    mockRejectValueOnce,
+  } = calledWith(...DefaultArgs);
+
+  Mock.mockReturnValueOnce = mockReturnValueOnce;
+  Mock.mockResolveValueOnce = mockResolveValueOnce;
+  Mock.mockRejectValueOnce = mockRejectValueOnce;
+  return Mock;
+}
+
+type Instance = {
+  [key: string]: (...args: any[]) => any;
+}
+type MockInstance<T extends Instance> = {
+  [key in keyof T]: Mock<Parameters<T[key]>, ReturnType<T[key]>>;
+}
+
+export function mock<T extends {}>(): T & MockInstance<T> {
+  const inner = {} as any;
+  return new Proxy({}, {
+    get(_, key, __) {
+      return inner[key] = inner[key] ?? createMock();
+    }
+  }) as any;
+}
